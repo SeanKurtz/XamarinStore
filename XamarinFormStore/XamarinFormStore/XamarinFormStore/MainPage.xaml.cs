a5a51833c76e2791f59564569a4cdd2e3593ed77
Sean Kurtz <sean_kurtz@hotmail.com>
2017-04-16 17:37:47 -0500

    Include Xamarin Forms store

---
+++ XamarinFormStore/XamarinFormStore/XamarinFormStore/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinFormStore
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+           
+        }
+        private async void Cart_Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new XamarinFormStore.CartPage());
+
+        }
+
+        private async void Store_Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new XamarinFormStore.StorePage());
+        }
+
+        private async void Options_Button_Clicked_1(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new XamarinFormStore.OptionsPage());
+        }
+
+        private async void Camera_Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new XamarinFormStore.CameraPage());
+        }
+    }
+}
---
+++ XamarinFormStore/XamarinFormStore/XamarinFormStore/ScannerPage.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace XamarinFormStore
+{
+    public class ScannerPage : ContentPage
+    {
+        ZXingScannerView scanView;
+        ZXingDefaultOverlay scanOverlay;
+        Grid resultGrid;
+        public ScannerPage() : base()
+        {
+            scanView = new ZXingScannerView
+            {
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.FillAndExpand,
+
+            };
+            scanView.OnScanResult += (result) =>
+               Device.BeginInvokeOnMainThread(async () =>
+               {
+                   scanView.IsAnalyzing = false;
+                   await DisplayAlert("Scanned Barcode", result.Text, "OK");
+                   await Navigation.PopAsync();
+
+               });
+            scanOverlay = new ZXingDefaultOverlay
+            {
+                TopText = "Hold your phone up to the barcode",
+                BottomText = "Scanning will happen automatically",
+                ShowFlashButton = scanView.HasTorch,
+            };
+            scanOverlay.FlashButtonClicked += (sender, e) =>
+            {
+                scanView.IsTorchOn = !scanView.IsTorchOn;
+            };
+
+            resultGrid = new Grid
+            {
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+            };
+            resultGrid.Children.Add(scanView);
+            resultGrid.Children.Add(scanOverlay);
+
+            Content = resultGrid;
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            scanView.IsScanning = true;
+        }
+        protected override void OnDisappearing()
+        {
+            scanView.IsScanning = false;
+            base.OnDisappearing();
+        }
+    }
+}
