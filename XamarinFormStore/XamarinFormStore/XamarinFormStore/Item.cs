dd991574ae75f8eb5bd9a7104c71c626962e9c2c
Sean Kurtz <sean_kurtz@hotmail.com>
2017-04-16 17:46:57 -0500

    Added temp data item

---
+++ XamarinFormStore/XamarinFormStore/XamarinFormStore/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamarinFormStore
+{
+    public class CartItem
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public double Price { get; set; }
+        public 
+    }
+}
