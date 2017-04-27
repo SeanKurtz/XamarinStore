74ba948bf9a1d0f2474c81997482fd4cd504869a
Sean Kurtz <sean_kurtz@hotmail.com>
2017-04-26 21:45:30 -0500

    Added Master Detail Page

--- XamarinFormStore/XamarinFormStore/XamarinFormStore/App.xaml.cs
+++ XamarinFormStore/XamarinFormStore/XamarinFormStore/App.xaml.cs
@@ -3,16 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Xamarin.Forms;
+using XamarinFormStore.Assets.Forms;
 
 namespace XamarinFormStore
 {
     public partial class App : Application
     {
-        //static ItemDatabase database;
+        static MasterDetailPage MasterPage;
+        static NavigationPage NavPage;
         public App()
         {
-            MainPage = new NavigationPage();
-            MainPage.Navigation.PushAsync(new XamarinFormStore.Assets.Forms.MainPage());
+            NavPage = new NavigationPage(new XamarinFormStore.Assets.Forms.MainPage());
+
+            MasterPage.Detail = NavPage;
+            MasterPage.Master = new XamarinFormStore.Assets.Forms.MainPage();
+
+            this.MainPage = MasterPage;
+            
         }
         /*public static ItemDatabase Database
         {
---
+++ XamarinFormStore/XamarinFormStore/XamarinFormStore/Assets/Forms/MenuPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinFormStore.Assets.Forms;
+
+namespace XamarinFormStore.Assets.Forms
+{
+    //[XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MenuPage : MasterDetailPage
+    {
+        public MenuPage()
+        {
+            InitializeComponent();
+        }
+        private async void Cart_Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new XamarinFormStore.Assets.Forms.CartPage());
+        }
+
+        private async void Store_Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new XamarinFormStore.Assets.Forms.StorePage());
+        }
+
+        private async void Options_Button_Clicked_1(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new XamarinFormStore.Assets.Forms.OptionsPage());
+        }
+
+        private async void Camera_Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new XamarinFormStore.Assets.Forms.ScannerPage());
+        }
+    }
+}
